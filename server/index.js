eb50e35acd9188397e3bd0c9f0701cdd85ed3a7b
Ufuk Bakan <println.ufukbakan@gmail.com>
2022-08-05 02:09:22 +0300

    Listen port 443

--- server/index.js
+++ server/index.js
@@ -3,13 +3,13 @@ const { createRoom, joinRoomWithId } = require('./src/rooms');
 
 const io = require('socket.io')({
     cors: {
-        origin: ["http://localhost", "http://localhost:80"],
+        origin: ["http://localhost", "https://localhost", "http://localhost:444", "http://localhost:443", "https://localhost:444"],
         methods: "*",
     },
     allowEIO3: true,
 });
 
-io.listen(9099);
+io.listen(443);
 
 io.on('connection', client => {
     client.on("request-create-room", (msg)=>{
